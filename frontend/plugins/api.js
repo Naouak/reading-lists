4ea3ba94d728de1f9cb050ba37330a50d716a9fd
Naouak <tardiel@gmail.com>
2020-05-17 22:51:12 +0200

    Add automatic log out on 401 from the API.

--- frontend/plugins/api.js
+++ frontend/plugins/api.js
@@ -1,9 +1,9 @@
-class Api{
-  constructor({$axios}){
+class Api {
+  constructor({$axios}) {
     this.$axios = $axios;
   }
 
-  bookRead(book){
+  bookRead(book) {
     return this.$axios.$post('/book/' + book.id + '/read/', {});
   }
 }
@@ -11,4 +11,12 @@ class Api{
 export default function (context, inject) {
   context.$api = new Api(context);
   inject('api', context.$api);
+
+  context.$axios.onError(error => {
+    const code = parseInt(error.response && error.response.status)
+    if (code === 401) {
+      context.$auth.logout();
+      context.$router.push('login');
+    }
+  })
 }
