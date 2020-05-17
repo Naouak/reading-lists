6cf02a1e65cc8e72a082c7e4e7f96169208d5830
Naouak <tardiel@gmail.com>
2020-05-17 23:06:24 +0200

    Nicer History

--- frontend/pages/history.vue
+++ frontend/pages/history.vue
@@ -2,7 +2,13 @@
   <section class="section">
     <h1 class="title">History</h1>
 
-    <ReadingHistoryEntry v-for="entry in entries" :key="entry.id" :entry="entry" @remove="remove" />
+    <div class="section" v-for="h in history" :key="h.date">
+      <h2 class="title">
+        {{h.date}}
+      </h2>
+      <ReadingHistoryEntry v-for="entry in h.entries" :key="entry.id" :entry="entry" @remove="remove" />
+    </div>
+
   </section>
 </template>
 
@@ -15,6 +21,22 @@ export default {
     return {
       entries: [],
     };
+  },
+
+  computed: {
+    history(){
+      return this.entries.reduce((acc, entry) => {
+        const date = new Date(entry.read_date);
+        const isodate = [date.getFullYear(),("0"+(date.getUTCMonth()+1)).substr(-2),("0"+date.getDate()).substr(-2)].join('-');
+        let current = acc[acc.length-1];
+        if(!current || current?.date !== isodate){
+          current = {date: isodate, entries: []};
+          acc.push(current);
+        }
+        current.entries.push(entry);
+        return acc;
+      }, []);
+    },
   },
 
   methods: {
