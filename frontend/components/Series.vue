29db8d142e7ff9e476601808fb65b72c41eb4ac5
Naouak <tardiel@gmail.com>
2020-06-12 20:26:08 +0200

    Better series page

---
+++ frontend/components/Series.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="box">
+    <nuxt-link :to="{name:'series-id-books', params: {id: series.id}}">
+      <div class="media">
+        <div class="media-left">
+          <figure style="width: 70px;">
+            <img style="width: auto; height: auto;" :src="series.cover_url" :alt="series.title">
+          </figure>
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <div class="subtitle">{{series.title}}</div>
+          </div>
+        </div>
+      </div>
+    </nuxt-link>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  components: {},
+  props: {
+    series: {
+      type: Object,
+    },
+  },
+};
+</script>
--- frontend/pages/series/index.vue
+++ frontend/pages/series/index.vue
@@ -1,31 +1,42 @@
 <template>
-  <div>
-    <h1>Series</h1>
-    <section class="section">
-      <Pagination :page="page" :previous-page-url="previousPageUrl" :next-page-url="nextPageUrl" />
-      <input type="search" v-model="search">
-      <div class="columns is-multiline">
-        <Card v-for="series in seriesList" :key="series.id" :title="series.title + '(' + series.book_count + ' books)' "
-              class="is-3">
-          <img :src="series.cover_url" />
-          <nuxt-link :to="{name:'series-id-books', params: {id: series.id}}">
-            GO TO
-          </nuxt-link>
-        </Card>
+  <section class="section">
+    <h1 class="title">Series</h1>
+    <div class="card">
+      <div class="card-content">
+        <Pagination :page="page" :previous-page-url="previousPageUrl" :next-page-url="nextPageUrl" />
       </div>
-    </section>
-  </div>
+      <div class="card-content">
+        <div class="field is-horizontal">
+          <div class="field-body">
+            <div class="field">
+              <p class="control has-icons-left">
+                <input v-model="search" type="text" class="input" placeholder="Filter" />
+                <span class="icon is-left"><b-icon icon="magnify" /></span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section class="card-content section columns is-multiline">
+        <div v-for="series in seriesList" :key="series.id" class="column is-6">
+          <Series :series="series" />
+        </div>
+      </section>
+    </div>
+
+  </section>
 </template>
 
 <script>
-import Card from '../../components/Card';
+import Series from '../../components/Series';
 import Pagination from "~/components/Pagination";
 
 export default {
-  name: 'Series',
+  name: 'SeriesPage',
   components: {
     Pagination,
-    Card,
+    Series,
   },
   data() {
     return {
