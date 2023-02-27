ff21ec8d9ff07b7be47f336d3e859dc49f7e0396
Naouak <tardiel@gmail.com>
2023-02-27 19:04:57 +0100

    Update Icon for Android homescreen

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -8,7 +8,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: "Comics Library",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -16,7 +16,8 @@ module.exports = {
         hid: 'description',
         name: 'description',
         content: process.env.npm_package_description || ''
-      }
+      },
+      { name: "theme-color", content: "#7957d5"}
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
