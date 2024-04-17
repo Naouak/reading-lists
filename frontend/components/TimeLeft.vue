8ff2990407d63d0c10c591dfcaa20819485e3740
Naouak <tardiel@gmail.com>
2024-04-18 00:05:44 +0200

    Add a statistic to show how long until everything is read.

---
+++ frontend/components/TimeLeft.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+  name: "TimeLeft",
+  props: {
+    time_left: {
+      default: 0
+    }
+  },
+  computed: {
+    time_left_human() {
+      let timeLeft = this.time_left;
+
+      if (timeLeft < 0) {
+        return "Done Already";
+      }
+
+      const humanDisplay = [];
+      if (timeLeft > 365) {
+        const years = timeLeft / 365 | 0;
+        humanDisplay.push(years, "years");
+        timeLeft -= years*365;
+      }
+      if (timeLeft > 30) {
+        const months = timeLeft / 30 | 0;
+        humanDisplay.push(months, "months");
+        timeLeft -= months * 30;
+      }
+      if (timeLeft > 0) {
+        humanDisplay.push(timeLeft | 0, "days");
+      }
+
+      if(humanDisplay.length === 0){
+        return "Never";
+      }
+
+      return humanDisplay.join(" ");
+    }
+  }
+};
+</script>
+
+<template>
+<span>
+  {{ time_left_human }}
+</span>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/pages/statistics.vue
+++ frontend/pages/statistics.vue
@@ -3,7 +3,8 @@
     <h1 class="title">Statistics</h1>
     <div class="box">
       <h2 class="subtitle">Total Progress</h2>
-      <Progress :value="read_progress">{{stats.read_books}} / {{stats.total_books}} read ({{read_progress}}%)</Progress>
+      <Progress :value="read_progress">{{ stats.read_books }} / {{ stats.total_books }} read ({{ read_progress }}%)
+      </Progress>
     </div>
 
     <div class="box">
@@ -12,15 +13,40 @@
       <div class="columns" style="text-align: center">
         <div class="column">
           <div class="subtitle">Read last week</div>
-          <div class="title">{{stats.read_last_week}} ({{read_last_week_progress}})</div>
+          <div class="title">{{ stats.read_last_week }} ({{ read_last_week_progress }})</div>
         </div>
         <div class="column">
           <div class="subtitle">Read last month</div>
-          <div class="title">{{stats.read_last_month}} ({{read_last_month_progress}})</div>
+          <div class="title">{{ stats.read_last_month }} ({{ read_last_month_progress }})</div>
         </div>
         <div class="column">
           <div class="subtitle">Read last year</div>
-          <div class="title">{{stats.read_last_year}} ({{read_last_year_progress}})</div>
+          <div class="title">{{ stats.read_last_year }} ({{ read_last_year_progress }})</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="box">
+      <h2 class="subtitle">Time to read up to <input type="number" v-model="target_percentage"
+                                                                             step="10" min="10" max="100">%</h2>
+      <div class="columns" style="text-align: center">
+        <div class="column">
+          <div class="subtitle">Time to finish (based on last week)</div>
+          <div class="title">
+            <TimeLeft :time_left="timeToRead(stats.read_last_week, 7, stats.total_books, stats.read_books, target_percentage / 100)" />
+          </div>
+        </div>
+        <div class="column">
+          <div class="subtitle">Time to finish (based on last month)</div>
+          <div class="title">
+            <TimeLeft :time_left="timeToRead(stats.read_last_month, 30, stats.total_books, stats.read_books, target_percentage / 100)" />
+          </div>
+        </div>
+        <div class="column">
+          <div class="subtitle">Time to finish (based on last year)</div>
+          <div class="title">
+            <TimeLeft :time_left="timeToRead(stats.read_last_year, 365, stats.total_books, stats.read_books, target_percentage / 100)" />
+          </div>
         </div>
       </div>
     </div>
@@ -31,33 +57,62 @@
       <div class="columns" style="text-align: center">
         <div class="column">
           <div class="subtitle">Added and available last week</div>
-          <div class="title">{{stats.available_last_week}} ({{available_last_week_progress}})</div>
+          <div class="title">{{ stats.available_last_week }} ({{ available_last_week_progress }})</div>
         </div>
         <div class="column">
           <div class="subtitle">Added and available last month</div>
-          <div class="title">{{stats.available_last_month}} ({{available_last_month_progress}})</div>
+          <div class="title">{{ stats.available_last_month }} ({{ available_last_month_progress }})</div>
         </div>
         <div class="column">
           <div class="subtitle">Added and available last year</div>
-          <div class="title">{{stats.available_last_year}} ({{available_last_year_progress}})</div>
+          <div class="title">{{ stats.available_last_year }} ({{ available_last_year_progress }})</div>
         </div>
       </div>
     </div>
+
+    <div class="box">
+      <h2 class="subtitle">Time to be up to date (target: <input type="number" v-model="target_percentage" step="10"
+                                                                 min="10" max="100">%)</h2>
+      <div class="columns" style="text-align: center">
+        <div class="column">
+          <div class="subtitle">Time to finish (based on last week)</div>
+          <div class="title">
+            <TimeLeft
+              :time_left="timeToReadEverything(stats.read_last_week, stats.available_last_week, 7, stats.total_books, stats.read_books, target_percentage / 100)" />
+          </div>
+        </div>
+        <div class="column">
+          <div class="subtitle">Time to finish (based on last month)</div>
+          <div class="title">
+            <TimeLeft
+              :time_left="timeToReadEverything(stats.read_last_month, stats.available_last_month, 30, stats.total_books, stats.read_books, target_percentage / 100)" />
+          </div>
+        </div>
+        <div class="column">
+          <div class="subtitle">Time to finish (based on last year)</div>
+          <div class="title">
+            <TimeLeft
+              :time_left="timeToReadEverything(stats.read_last_year, stats.available_last_year, 365, stats.total_books, stats.read_books, target_percentage / 100)" />
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="box">
       <h2 class="subtitle">Catalog progress</h2>
 
       <div class="columns" style="text-align: center">
         <div class="column">
           <div class="subtitle">Added last week</div>
-          <div class="title">{{stats.added_last_week}} ({{added_last_week_progress}})</div>
+          <div class="title">{{ stats.added_last_week }} ({{ added_last_week_progress }})</div>
         </div>
         <div class="column">
           <div class="subtitle">Added last month</div>
-          <div class="title">{{stats.added_last_month}} ({{added_last_month_progress}})</div>
+          <div class="title">{{ stats.added_last_month }} ({{ added_last_month_progress }})</div>
         </div>
         <div class="column">
           <div class="subtitle">Added last year</div>
-          <div class="title">{{stats.added_last_year}} ({{added_last_year_progress}})</div>
+          <div class="title">{{ stats.added_last_year }} ({{ added_last_year_progress }})</div>
         </div>
       </div>
     </div>
@@ -74,14 +129,21 @@
 <script>
 
 import Progress from "~/components/Progress.vue";
+import TimeLeft from "~/components/TimeLeft";
+
 
 export default {
   name: "Statistics",
-  components: {Progress},
-  asyncData({$axios}) {
-    return $axios.$get('/reading-statistics/').then(result => {
+  components: { TimeLeft, Progress },
+  data() {
+    return {
+      "target_percentage": 100
+    };
+  },
+  asyncData({ $axios }) {
+    return $axios.$get("/reading-statistics/").then(result => {
       return {
-        'stats': result,
+        "stats": result
       };
     });
   },
@@ -151,8 +213,33 @@ export default {
     },
     read_progress() {
       return ((this.stats.read_books / this.stats.total_books * 10000) | 0) / 100;
-    },
+    }
   },
+  methods: {
+    timeToRead(read, delta, total, done, targetRatio) {
+      const target = total * targetRatio;
+      const left = target - done;
+      return left / read * delta;
+    },
+    timeToReadEverything(read, added, delta, total, done, targetRatio) {
+      // If we don't read fast enough to catch up
+      if (read < added * targetRatio) {
+        return 0;
+      }
 
-}
+      const target = total * targetRatio;
+      const left = target - done;
+      let daysToTarget = left / read * delta;
+
+      // We got to the target but the time it took to get there, the catalog increased too
+      if (daysToTarget > delta) {
+        const addedDuring = daysToTarget / delta * added;
+        daysToTarget += this.timeToReadEverything(read, added, delta, total + addedDuring, target, targetRatio);
+      }
+
+      return daysToTarget;
+    }
+  }
+
+};
 </script>
